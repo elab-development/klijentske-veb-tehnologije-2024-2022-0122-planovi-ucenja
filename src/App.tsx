1d8c901a24ba0c089fd00239dcb6e47294714c65
NemanjaMiticc <nemanjamiticzv@gmail.com>
2024-08-24 17:35:17 +0200

    Dodata komponenta za pojedinacan task

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import Ispitnirokovi from './components/Ispitnirokovi'
 import Kontakt from './components/Kontakt'
 import Navbar from './components/Navbar'
+import Task from './components/Task'
 
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <Ispitnirokovi/>
+      {/* <Navbar/> */}
+      <Task/>
+      {/* <Ispitnirokovi/> */}
       {/* <Kontakt/> */}
     </>
   )
---
+++ src/components/Task.tsx
@@ -0,0 +1,14 @@
+import './Task.css'
+
+const Task = () => {
+  return (
+    <div className='Task-container'>
+        <p>Task1</p>
+        <p>KTEH</p>
+        <p>14:00h-18:00h</p>
+        <div className='cekiranjeDiv'></div>
+    </div>
+  )
+}
+
+export default Task
